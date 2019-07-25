8e492c84842f61afd73f29d00994c42ec09c7139
mirrormingzZ <1060996414@qq.com>
2019-07-25 17:34:34 +0800

    'init'

---
+++ src/main/java/cn/mirror/concurrency/ConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package cn.mirror.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyApplication.class, args);
+    }
+
+}
